658c0f7b73b8433340d26713073520063c5d518b
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-12-21 13:04:29 +0530

    TreeMap

--- Java_Collections/Map/TreeMap/TreeMaps.java
+++ Java_Collections/Map/TreeMap/TreeMaps.java
@@ -5,6 +5,11 @@ import java.util.TreeMap;
 public class TreeMaps {
     public static void main(String[] args) {
         TreeMap<Integer,String> tm = new TreeMap<>();
-        tm.put(1,"Ayush");
+        tm.put(12,"Ayush");
+        tm.put(7,"Singh");
+        tm.put(1,"Tomar");
+        System.out.println(tm);
+        System.out.println(tm.floorKey(6));
+        System.out.println(tm.ceilingKey(6));
     }
 }
